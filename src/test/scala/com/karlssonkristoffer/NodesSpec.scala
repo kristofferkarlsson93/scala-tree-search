feaec3852b0573e9081b02b20b6482c56f94001c
Kristoffer Karlsson <karlsson.kristoffer93@gmail.com>
2021-06-20 14:50:18 +0200

    Adding second simple test. Did not fail

--- src/test/scala/com/karlssonkristoffer/NodesSpec.scala
+++ src/test/scala/com/karlssonkristoffer/NodesSpec.scala
@@ -13,5 +13,12 @@ class NodesSpec extends Spec {
       val result = Nodes.getCommonNodeNamesExceptBepa(tree, tree)
       result shouldBe Seq.empty
     }
+    "should return an empty list when the three only contain nodes with name 'Bepa'" in {
+      val nodeInfo = randomOf(genNodeInfo).copy(nodeInfoName = NodeName("Bepa"))
+      val tree = Tree(nodeInfo, Seq.empty)
+
+      val result = Nodes.getCommonNodeNamesExceptBepa(tree, tree)
+      result shouldBe Seq.empty
+    }
   }
 }
