7f717988b04a71c1d80565cdd244f3b8edeb131b
Kristoffer Karlsson <karlsson.kristoffer93@gmail.com>
2021-06-21 21:38:21 +0200

    starting with properby tests

--- src/test/scala/com/karlssonkristoffer/NodesPropertySpec.scala
+++ src/test/scala/com/karlssonkristoffer/NodesPropertySpec.scala
@@ -9,11 +9,11 @@ class NodesPropertySpec extends Spec with ScalaCheckPropertyChecks {
     "for any 2 trees should return common nodes" in {
       forAll(GenTreeOfDept(randomOf(intBetween1And100)), GenTreeOfDept(randomOf(intBetween1And100))) { (firstTree, secondTree) =>
         val secondTreeHasCommonNodeWithFirstTree = {
-          val secondTreeNodes = secondTree.listNodes()
-          firstTree.listNodes().exists(n => secondTreeNodes.contains(n))
+          val secondTreeNodes = secondTree.listNodes().map(_.nodeInfoName)
+          firstTree.listNodes().map(_.nodeInfoName).exists(n => secondTreeNodes.contains(n))
         }
         whenever(secondTreeHasCommonNodeWithFirstTree) {
-
+          true shouldBe true
         }
       }
     }
